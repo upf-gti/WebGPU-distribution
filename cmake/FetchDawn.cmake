a84e3966e9771751bdc0d714ae3266cfe912b4b8
PZerua <pachus.wtf@gmail.com>
2023-09-22 17:02:31 +0200

    Update dawn repo & always use vulkan

--- cmake/FetchDawn.cmake
+++ cmake/FetchDawn.cmake
@@ -15,7 +15,7 @@ FetchContent_Declare(
 	DOWNLOAD_COMMAND
 		cd ${FETCHCONTENT_BASE_DIR}/dawn-src &&
 		git init &&
-		git pull --depth=1 https://github.com/blitz-research/dawn openxr-dev &&
+		git pull --depth=1 https://github.com/upf-gti/dawn openxr-dev &&
 		git reset --hard FETCH_HEAD
 )
 
@@ -23,26 +23,20 @@ FetchContent_GetProperties(dawn)
 if (NOT dawn_POPULATED)
 	FetchContent_Populate(dawn)
 
-	find_package(PythonInterp 3 REQUIRED)
+	set(DAWN_FETCH_DEPENDENCIES ON)
 
-	message(STATUS "Running fetch_dawn_dependencies:")
-	execute_process(
-		COMMAND ${PYTHON_EXECUTABLE} "${CMAKE_CURRENT_SOURCE_DIR}/tools/fetch_dawn_dependencies.py"
-		WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/_deps/dawn-src"
-	)
+	# if (APPLE)
+	# 	set(USE_METAL ON)
+	# elseif (WIN32)
+	# 	set(USE_DX12 ON)
+	# 	target_compile_definitions(webgpu INTERFACE BACKEND_DX12)
+	# else()
+	# 	set(USE_VULKAN ON)
+	# 	target_compile_definitions(webgpu INTERFACE BACKEND_VULKAN)
+	# endif()
 
-	if (APPLE)
-		set(USE_METAL ON)
-	elseif (WIN32)
-		set(USE_DX12 ON)
-		target_compile_definitions(webgpu INTERFACE BACKEND_DX12)
-	else()
-		set(USE_VULKAN ON)
-		target_compile_definitions(webgpu INTERFACE BACKEND_VULKAN)
-	endif()
-
-	# set(USE_VULKAN ON)
-	# target_compile_definitions(webgpu INTERFACE BACKEND_VULKAN)
+	set(USE_VULKAN ON)
+	target_compile_definitions(webgpu INTERFACE BACKEND_VULKAN)
 
 	set(DAWN_ENABLE_METAL ${USE_METAL})
 	set(DAWN_ENABLE_D3D12 ${USE_DX12})
@@ -84,7 +78,6 @@ set(AllDawnTargets
 	dawn_proc
 	dawn_utils
 	dawn_wire
-	dawn_static_deps
 	dawncpp
 	dawncpp_headers
 	emscripten_bits_gen
@@ -92,8 +85,7 @@ set(AllDawnTargets
 	extinst_tables
 	webgpu_dawn
 	webgpu_headers_gen
-	libtint
-	tint_diagnostic_utils
+	tint_api
 	tint_utils_io
 	tint_val
 	tint-format
@@ -106,8 +98,27 @@ set(AllGlfwTargets
 	uninstall
 )
 
+function(filter_all_targets var)
+    set(targets)
+    get_all_targets_recursive(targets ${CMAKE_CURRENT_SOURCE_DIR})
+    set(${var} ${targets} PARENT_SCOPE)
+endfunction()
+
+macro(get_all_targets_recursive targets dir)
+    get_property(subdirectories DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
+    foreach(subdir ${subdirectories})
+        get_all_targets_recursive(${targets} ${subdir})
+    endforeach()
+
+    get_property(current_targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
+	set_property(TARGET ${current_targets} PROPERTY FOLDER "External/")
+endmacro()
+
+filter_all_targets(all_targets)
+
 foreach (Target ${AllDawnTargets})
 	if (TARGET ${Target})
+		message(STATUS ${Target})
 		set_property(TARGET ${Target} PROPERTY FOLDER "External/Dawn")
 	endif()
 endforeach()
